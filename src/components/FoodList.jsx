000f2766aa42b81c3e24fdb9fbe85c7def8a30ce
ElDav1d <dvivoes@yahoo.es>
2023-03-01 19:20:15 +0100

    Resolves Iteration 8

--- src/components/FoodList.jsx
+++ src/components/FoodList.jsx
@@ -4,23 +4,27 @@ function FoodList({ dishes, searchInput, setDishes }) {
   return (
     <>
       <h2>FoodList</h2>
-      <ul>
-        {dishes
-          .filter((dish) =>
-            dish.name.toLowerCase().includes(searchInput.toLowerCase())
-          )
-          .map(({ name, image, calories, servings }, index) => (
-            <FoodBox
-              name={name}
-              image={image}
-              calories={calories}
-              servings={servings}
-              key={name + index}
-              setDishes={setDishes}
-              index={index}
-            />
-          ))}
-      </ul>
+      {dishes.length ? (
+        <ul>
+          {dishes
+            .filter((dish) =>
+              dish.name.toLowerCase().includes(searchInput.toLowerCase())
+            )
+            .map(({ name, image, calories, servings }, index) => (
+              <FoodBox
+                name={name}
+                image={image}
+                calories={calories}
+                servings={servings}
+                key={name + index}
+                setDishes={setDishes}
+                index={index}
+              />
+            ))}
+        </ul>
+      ) : (
+        <h3>Oooops! There is no more content to show</h3>
+      )}
     </>
   );
 }
